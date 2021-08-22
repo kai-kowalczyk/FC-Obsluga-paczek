a512e5738b2c7902180cd92707d543122e248441
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-08-22 12:44:33 +0200

    created a loop asking for elements' weight input

--- paczki.py
+++ paczki.py
@@ -5,3 +5,12 @@ numer_paczki = 0
 waga_paczki = 0
 wyslane_kilogramy = 0
 puste_kilogramy = numer_paczki * 20 - wyslane_kilogramy
+
+for numer_elementu in range(liczba_elementow):
+    waga_elementu = int(input('Podaj wagę elementu w kilogramach: '))
+    if waga_elementu == 0:
+        print ('Błąd: Waga elementu nie może wynosić 0.')
+        break
+    elif waga_elementu < 1 or waga_elementu > 10:
+        print('Błąd: Waga paczki musi mieścić się w zakresie 1-10 kg.')
+        break
