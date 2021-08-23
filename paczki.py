2b9584dee867be62230f91c9289d5022d6fcda8e
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-08-23 13:57:55 +0200

    added a few lines of code

--- paczki.py
+++ paczki.py
@@ -9,8 +9,13 @@ puste_kilogramy = numer_paczki * 20 - wyslane_kilogramy
 for numer_elementu in range(liczba_elementow):
     waga_elementu = int(input('Podaj wagę elementu w kilogramach: '))
     if waga_elementu == 0:
-        print ('Błąd: Waga elementu nie może wynosić 0.')
         break
     elif waga_elementu < 1 or waga_elementu > 10:
         print('Błąd: Waga paczki musi mieścić się w zakresie 1-10 kg.')
         break
+    else:
+        wyslane_kilogramy += waga_elementu
+        if waga_paczki + waga_elementu <= 20:
+            waga_paczki += waga_elementu
+        else:
+            numer_paczki += 1
